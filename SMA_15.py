7aad99f876d21239b9533b11333516d35109f522
Hexal <hexalations@gmail.com>
2017-11-07 16:12:22 +0000

    Simple Moving Average Trading Strategy
    Uses - PyAlgoTrade

---
+++ SMA_15.py
@@ -0,0 +1,73 @@
+# A simple algo trading moving average strategy, this time simulating actual trading. The idea is very simple:
+
+#The first thing that we’ll need to test our strategies is some data.
+
+# Let’s use Oracle’s stock prices for year 2000, which we’ll download with the following command:
+
+python -c "from pyalgotrade.tools import yahoofinance; yahoofinance.download_daily_bars('orcl', 2000, 'orcl-2000.csv')"
+
+
+If the adjusted close price is above the SMA(15) we enter a long position (we place a buy market order).
+If a long position is in place, and the adjusted close price drops below the SMA(15)
+we exit the long position (we place a sell market order).
+
+from pyalgotrade import strategy
+from pyalgotrade.barfeed import yahoofeed
+from pyalgotrade.technical import ma
+
+
+class MyStrategy(strategy.BacktestingStrategy):
+    def __init__(self, feed, instrument, smaPeriod):
+        super(MyStrategy, self).__init__(feed, 1000)
+        self.__position = None
+        self.__instrument = instrument
+        # We'll use adjusted close values instead of regular close values.
+        self.setUseAdjustedValues(True)
+        self.__sma = ma.SMA(feed[instrument].getPriceDataSeries(), smaPeriod)
+
+    def onEnterOk(self, position):
+        execInfo = position.getEntryOrder().getExecutionInfo()
+        self.info("BUY at $%.2f" % (execInfo.getPrice()))
+
+    def onEnterCanceled(self, position):
+        self.__position = None
+
+    def onExitOk(self, position):
+        execInfo = position.getExitOrder().getExecutionInfo()
+        self.info("SELL at $%.2f" % (execInfo.getPrice()))
+        self.__position = None
+
+    def onExitCanceled(self, position):
+        # If the exit was canceled, re-submit it.
+        self.__position.exitMarket()
+
+    def onBars(self, bars):
+        # Wait for enough bars to be available to calculate a SMA.
+        if self.__sma[-1] is None:
+            return
+
+        bar = bars[self.__instrument]
+        # If a position was not opened, check if we should enter a long position.
+        if self.__position is None:
+            if bar.getPrice() > self.__sma[-1]:
+                # Enter a buy market order for 10 shares. The order is good till canceled.
+                self.__position = self.enterLong(self.__instrument, 10, True)
+        # Check if we have to exit the position.
+        elif bar.getPrice() < self.__sma[-1] and not self.__position.exitActive():
+            self.__position.exitMarket()
+
+
+def run_strategy(smaPeriod):
+    # Load the yahoo feed from the CSV file
+    feed = yahoofeed.Feed()
+    feed.addBarsFromCSV("orcl", "orcl-2000.csv")
+
+    # Evaluate the strategy with the feed.
+    myStrategy = MyStrategy(feed, "orcl", smaPeriod)
+    myStrategy.run()
+    print "Final portfolio value: $%.2f" % myStrategy.getBroker().getEquity()
+
+run_strategy(15)
+
+
+## the next step is to optimise for the parameter of the SMA period length - not necessarily optimal at 15
